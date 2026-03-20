24dcaf342360fe78ac2abf117a96bf06423505bf
LudekFiser <breadbeats@seznam.cz>
2026-03-21 00:03:27 +0100

    Created demo for my first Spring Boot cli helper, which is supposted to create folder structure for me + write into .yml config so that i dont have to :D

---
+++ src/main.rs
@@ -0,0 +1,96 @@
+use std::error::Error;
+use std::fs;
+use std::fs::{File};
+use std::io::{Write};
+use std::path::Path;
+use walkdir::WalkDir;
+use dialoguer::Select;
+fn main() {
+    //TODO instead of find_file_by_name use Path/PathBuff
+    let paths = find_file_by_name("./", "java.rs");
+    if !paths.is_empty() {
+        for path in paths {
+            let path_without_suffix = path.strip_suffix("java.rs").unwrap();
+            create_folders(path_without_suffix).expect("Failed to create folders");
+
+            write_into_yml_file();
+
+
+            println!("{}", path_without_suffix);
+        }
+    }
+}
+
+fn get_yml_file() -> File {
+    //TODO instead of find_file_by_name use Path/PathBuff
+    let db_properties_file_path = find_file_by_name("./", "application.yml");
+    let stringed = db_properties_file_path.join("");
+    File::create(&stringed).expect(&format!("Failed to open file: {:?}", stringed))
+}
+
+fn write_into_yml_file() {
+    let mut file = get_yml_file();
+    let chosen_db = ask_user_for_db();
+
+    let url: String;
+    if chosen_db == "PG" {
+        url = "jdbc:postgresql://localhost:5432/{DB_NAME}".to_string();
+    } else {
+        url = "jdbc:mysql://localhost:3306/{DB_NAME}".to_string();
+    }
+
+    let yml_content = format!("spring:\n\
+                                \tdatasource:\n\
+                                    \t\turl: {url}");
+    file.write_all(yml_content.as_ref()).expect("gaga")
+}
+
+// TODO add enums instead of vec mf
+fn ask_user_for_db()  -> &'static str {
+    let items = vec!["[1] - PostgreSQL (Default)", "[2] - MySQL"];
+
+    let selection = Select::new()
+        .with_prompt("Choose a Database")
+        .items(&items)
+        .default(0)
+        .interact()
+        .unwrap();
+    let choice = &selection;
+    if *choice == 0 {
+        "PG"
+    } else {
+        "MSQL"
+    }
+}
+
+
+
+// find main.java to get the path
+fn find_file_by_name(root: &str, filename: &str) -> Vec<String> {
+    WalkDir::new(root)
+        .into_iter()
+        .filter_map(|entry| entry.ok())
+        .filter(|entry| entry.file_name().to_string_lossy() == filename)
+        .map(|entry| entry.path().to_string_lossy().into_owned())
+        .collect()
+}
+
+// create folders
+fn create_folders(root: &str) -> Result<&str, Box<dyn Error>> {
+    let folders_to_create = vec![
+        "config", "controller", "repository", "entity",
+        "mapper", "service", "exception", "service/impl"];
+    if root.is_empty() {
+        return Err(Box::from("Path is empty!"));
+    }
+
+    for folder in folders_to_create {
+        let name = Path::new(root).join(folder);
+        if !name.exists() {
+            fs::create_dir(name)?;
+        }
+    }
+
+    Ok("Folders Successfully created!")
+}
+
