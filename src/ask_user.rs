45248a0bc4222d99e5d37cd85a458ebe39ebbca3
LudekFiser <breadbeats@seznam.cz>
2026-03-22 01:01:31 +0100

    cleanup

--- src/ask_user.rs
+++ src/ask_user.rs
@@ -7,8 +7,8 @@ pub(crate) enum DbChoice {
 }
 
 pub(crate) fn ask_user_for_db() -> Result<DbChoice, Box<dyn Error>> {
-    let items = vec!["[1] - PostgreSQL (Default)", "[2] - MySQL"];
-    
+    let items = vec!["[1] - PostgreSQL", "[2] - MySQL"];
+
     let selection = Select::new()
         .with_prompt("Choose a Database")
         .items(&items)
